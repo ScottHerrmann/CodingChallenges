eb5994ab391a6157f93a7e4d2c927c871d55701a
Scott Herrmann <khamarr3524@gmail.com>
2018-01-16 00:07:13 -0600

    Added Default Arguments and Private Members

--- Class/class.lua
+++ Class/class.lua
@@ -5,9 +5,12 @@
 --Deallocate memory
 --Delete Classes
 --Delete method(s)
-
+--Make classes their own object constructors
 Class = {}
 Class.__classes = {}
+
+local minPrefix = 3
+local maxPrefix = 8
 
 local classExists = function(name)
   return(type(Class.__classes[name]) == "table")
@@ -17,12 +20,42 @@ local methodExists = function(name, fxn)
   return(type(Class.__classes[name][fxn]) == "function")
 end
 
+local generatePrivatePrefix = function()
+  local ranLen = math.floor(math.random(minPrefix, maxPrefix))
+  local out = ""
+  local validChars = {
+    {65, 90}, --Captial letters
+    {97, 122}, --lowercase letters
+    {48, 57} -- numbers
+  }
+  local first = math.floor(math.random(1, 2))
+  out = out .. string.char(math.floor(math.random(validChars[first][1], validChars[first][2])))
+  for i = 2, ranLen do
+    local setIndex = math.floor(math.random(1, 3))
+    out = out .. string.char(math.floor(math.random(validChars[setIndex][1], validChars[setIndex][2])))
+  end
+  return out
+end
+
+
 --Name of the class, ARRAY of member NAMES
-Class.newClass = function(name, args)
+Class.newClass = function(name, args, defaults, bPrivate)
   if not classExists(name) then
     local t = {}
     setmetatable(t, {__index = Class})
+    t.private = bPrivate
+    if bPrivate == true then
+      local prefix = generatePrivatePrefix()
+      for k, v in pairs(args) do
+        local tmp = prefix .. v
+        args[k] = tmp
+      end
+      t.__prefix = prefix
+    end
     t.args = args
+    if type(defaults) == "table" then
+      t.defaultArgs = defaults
+    end
     Class.__classes[name] = t
   else
     assert(false, "Class " .. name .. " is already defined.")
@@ -32,12 +65,27 @@ end
 --Name of the class, ARRAY of member VALUES
 Class.new = function(name, args)
   if classExists(name) then
+    local argCount = #Class.__classes[name].args 
+    local inArgCount = #args
+    local memberNames = Class.__classes[name].args
+    local defaults = Class.__classes[name].defaultArgs
     local t = {}
     setmetatable(t, { 
         __index = Class.__classes[name],
-        __tostring = function(t) return name end})
-    for k, v in ipairs(Class.__classes[name].args) do
-      t[v] = args[k]
+        __tostring = function(t) return name end,
+        __newindex = Class.__classes[name]})
+    for i = 1, argCount do
+      if defaults ~= nil and (args[i] == nil or args[i] == "") then
+        if defaults[i] then
+          t[memberNames[i]] = defaults[i]
+        else
+          assert(false, "Attempted to index default value for " .. memberNames[i] .. ". No default set.")
+        end
+      elseif defaults == nil and (args[i] == nil or args[i] == "") then
+        assert(false, "Attempted to index default value for " .. memberNames[i] .. ". No default set.")
+      else
+        t[memberNames[i]] = args[i]
+      end
     end
     if type(t.init) == "function" then
       t.init()
--- Class/main.lua
+++ Class/main.lua
@@ -1,6 +1,7 @@
-function love.load()
+function love.load(dir, mode)
   require("class")
-  Class.newClass("Test", {"name", "value"})
+  math.randomseed(os.time())
+  Class.newClass("Test", {"name", "value"}, {}, true)
   local fxn = function(self, someValue)
     local out = ""
     for k, v in pairs(self) do
@@ -11,7 +12,12 @@ function love.load()
     return out .. ":" .. someValue
   end
   Class.newMethod("Test", "GetAllData", fxn)
-  Test = Class.new("Test", {"Hello World", 12345})
+  Class.newMethod("Test", "getName", function(self) return self[self.__prefix .. "name"] end)
+  Test = Class.new("Test", {"TEST 123", 12345})
+  Class.newClass("Test2", {"name", "value"}, {"Default Name", 12342563})
+  Test2 = Class.new("Test2", {"Not Default Name"})
+  print(math.random(1, 2))
+  
 end
 
 
@@ -19,8 +25,10 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print(Test.name, 0, 0)
-  love.graphics.print(Test.value, 0, 20)
+  love.graphics.print(Test:getName(), 0, 0)
+  --love.graphics.print(Test.value, 0, 20)
   love.graphics.print(Test:GetAllData(25), 0, 40)
   love.graphics.print(tostring(Test), 0, 60)
+  love.graphics.print(Test2.name, 0, 80)
+  love.graphics.print(Test2.value, 0, 100)
 end
